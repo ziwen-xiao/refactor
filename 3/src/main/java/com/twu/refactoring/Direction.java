a7c5051816c134f262ca26b46aed4ddd1ec2b5b9
xiao ziwen <ziwen.xiao@thoughtworks.com>
2021-08-14 17:43:16 +0800

    [xiaoziwen] feat：完成题3部分重构

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -7,6 +7,10 @@ public class Direction {
         this.direction = direction;
     }
 
+    public char getDirection() {
+        return direction;
+    }
+
     public Direction turnRight() {
         switch (direction) {
             case 'N':
@@ -37,25 +41,4 @@ public class Direction {
         }
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Direction direction1 = (Direction) o;
-
-        if (direction != direction1.direction) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) direction;
-    }
-
-    @Override
-    public String toString() {
-        return "Direction{direction=" + direction + '}';
-    }
 }
--- 3/src/test/java/com/twu/refactoring/DirectionTest.java
+++ 3/src/test/java/com/twu/refactoring/DirectionTest.java
@@ -1,5 +1,6 @@
 package com.twu.refactoring;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,18 +21,19 @@ public class DirectionTest {
     @Test
     public void shouldTurnEastWhenTurnRightFromNorth() {
         Direction east = north.turnRight();
-        assertThat(east, is(new Direction('E')));
+        Assertions.assertEquals('E', east.getDirection());
     }
 
     @Test
     public void shouldTurnWestWhenTurnLeftFromNorth() {
         Direction west = north.turnLeft();
-        assertThat(west, is(new Direction('W')));
+        Assertions.assertEquals('W', west.getDirection());
+
     }
 
     @Test
     public void shouldTurnNorthWhenTurnLeftFromEast() {
         Direction north = east.turnLeft();
-        assertThat(north, is(new Direction('N')));
+        Assertions.assertEquals('N', north.getDirection());
     }
 }
